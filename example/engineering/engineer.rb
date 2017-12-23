e0e3b371e4194127c2b164a9bcedcd7ae8262ea2
Luke Morton <lukemorton.dev@gmail.com>
2017-12-23 20:06:12 +0000

    Add as: keyword

--- example/engineering/engineer.rb
+++ example/engineering/engineer.rb
@@ -1,5 +1,5 @@
 JobSpec::Role.definition 'Engineer' do
-  include CoreEngineerExpectations
+  include CoreEngineerExpectations, as: 'Core Expectations'
 
   expected 'to be cool', 'Because it that is good'
 end
