72e0b2ec2ce91db9153b68b2fed2d62c5f80cf14
Luke Morton <lukemorton.dev@gmail.com>
2017-12-23 18:09:03 +0000

    Finish saving of files to markdown

--- lib/job_spec/cli.rb
+++ lib/job_spec/cli.rb
@@ -3,12 +3,21 @@ require 'job_spec'
 
 module JobSpec
   class CLI < Thor
-    desc 'build DIR', 'build roles in DIR'
-    def build(path = '.')
-      print "Building roles in #{path}... "
-      Dir[File.join(Dir.pwd, path, '**/*.rb')].each { |f| require f }
-      puts 'done.'
-      p Role.definitions
+    desc 'build', 'build roles in DIR out to DIR'
+    option :in, required: true
+    option :out, required: true
+    def build
+      puts "Looking in '#{options[:in]}' for roles... "
+      role_files(options[:in]).each do |f|
+        require f
+      end
+
+      FileUtils.mkdir_p(path_relative_to_pwd(options[:out]))
+      Role.definitions.each do |role|
+        puts "Saving #{role.name} to #{safe_role_out_path(role)}..."
+        File.write(safe_role_out_path(role), RenderAsMarkdown.new(role).render)
+      end
+      puts 'Finished.'
     end
 
     map %w(--version -v) => :version
@@ -18,4 +27,24 @@ module JobSpec
       puts "jobspec v#{JobSpec::VERSION}"
     end
   end
+end
+
+def path_relative_to_pwd(path)
+  File.expand_path(path, Dir.pwd)
+end
+
+def path_glob(dir)
+  path_relative_to_pwd(File.join(dir, '**/*.rb'))
+end
+
+def role_files(dir)
+  Dir[path_glob(dir)]
+end
+
+def safe_role_name(role)
+  "#{role.name.downcase.gsub(/[^a-z]+/, '')}.md"
+end
+
+def safe_role_out_path(role)
+  File.join(path_relative_to_pwd(options[:out]), safe_role_name(role))
 end
